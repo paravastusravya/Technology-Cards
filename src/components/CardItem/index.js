4d57d16abe4ebeddfeea43e9ce42421d539d6da7
paravastusravya <paravastusravya@gmail.com>
2023-01-30 07:37:48 +0000

    commit

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,18 @@
+// Write your code here.
+import './index.css'
+
+const CardItem = props => {
+  const {cardDetails} = props
+  const {title, description, imgUrl, className} = cardDetails
+  return (
+    <li className={`li-container ${className}`}>
+      <h1 className="title">{title}</h1>
+      <p className="description">{description}</p>
+
+      <div className="image-container">
+        <img src={imgUrl} className="image" alt={title} />
+      </div>
+    </li>
+  )
+}
+export default CardItem
